7c798034352cb5dbed0941ec1bd5cf3836d46ee3
Gourav-21 <rahul.dora21@gmail.com>
2024-11-09 18:23:17 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,131 @@
-import Image from "next/image";
+'use client'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
-export default function Home() {
+interface Todo {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [editingId, setEditingId] = useState<number | null>(null)
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleAddTodo(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, { task: inputValue, completed: false, id: Math.floor(Math.random() * 100000000) }]);
+      setInputValue("");
+    }
+  }
+
+  function toggleTask(index: number) {
+    const updatedTodos = todos.map((todo, todoIndex) =>
+      todo.id === index ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  }
+
+  const handleDeleteTodo = (index: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== index); // Remove the selected todo
+    setTodos(updatedTodos); // Update the state with the remaining todos
+  };
+
+  const startEditing = (id: number) => {
+    setEditingId(id)
+  }
+
+  const finishEditing = (id: number, newTitle: string) => {
+    setTodos(todos.map(todo => 
+      todo.id == id ? { ...todo, task: newTitle.trim() } : todo
+    ))
+    setEditingId(null)
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="flex h-screen items-center justify-center">
+      <div>
+        <CardHeader>
+          <CardTitle className="text-3xl text-center">Todo App</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-2">
+          <form onSubmit={handleAddTodo} className="flex gap-2 ">
+            <Input
+              type="text"
+              placeholder="Add your task"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <Button>Submit</Button>
+          </form >
+          <div>
+            <ul>
+              {todos.map((todo, index) => (
+                <TodoItem
+                  key={todo.id}
+                  todo={todo}
+                  toggleTask={toggleTask}
+                  startEditing={startEditing}
+                  finishEditing={finishEditing}
+                  handleDeleteTodo={handleDeleteTodo}
+                  editingId={editingId}
+                />
+              ))}
+            </ul>
+          </div>
+        </CardContent>
+      </div>
     </div>
+  );
+}
+
+export default App;
+
+function TodoItem({
+  todo,
+  toggleTask,
+  startEditing,
+  finishEditing,
+  handleDeleteTodo,
+  editingId
+}: {
+  todo: Todo;
+  toggleTask: (index: number) => void;
+  startEditing: (index: number) => void;
+  finishEditing: (index: number, newTitle: string) => void;
+  handleDeleteTodo: (index: number) => void;
+  editingId: number | null
+}) {
+  return (
+    <Card className={`p-3 my-2 flex items-center justify-between ${todo.completed ? 'border-2 border-green-500' : ''}`}>
+      <div className="flex items-center gap-2">
+        <Checkbox
+          checked={todo.completed}
+          onCheckedChange={() => toggleTask(todo.id)}
+          className={`h-5 w-5 ${todo.completed ? 'text-white' : ''}`}
+        />
+        {editingId === todo.id ? (
+          <Input
+            type="text"
+            defaultValue={todo.task}
+            onBlur={(e) => finishEditing(todo.id, e.target.value)}
+            onKeyPress={(e) => e.key === 'Enter' && finishEditing(todo.id, e.currentTarget.value)}
+            className="flex-grow"
+            autoFocus
+          />
+        ) : (
+          <Label onClick={() => startEditing(todo.id)} htmlFor="text">{todo.task}</Label>
+        )}
+      </div>
+      <div className="cursor-pointer" onClick={() => handleDeleteTodo(todo.id)}>
+        ❌
+      </div>
+    </Card>
   );
 }
